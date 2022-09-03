22f864873a2f0788671a92dbeb9e6bc8353969e7
Mor Khojasteh <mori.kh1987@gmail.com>
2022-09-03 14:13:47 +0100

    feat: added fetchApi file for fetch data from rapidapi.com

---
+++ utils/fetchApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com";
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      "X-RapidAPI-Key": "df97e8d387msh4c81fe929b01237p19ef31jsn64e4e91019af",
+      "X-RapidAPI-Host": "bayut.p.rapidapi.com",
+    },
+  });
+
+
+
+  return data;
+};
