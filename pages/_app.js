cf725d7056d7c3b0b69f8402afbcfc8f6aee3c41
Mor Khojasteh <mori.kh1987@gmail.com>
2022-09-04 12:49:28 +0100

    feat: added layout to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import Router from "next/router";
+import Head from "next/head";
+import NProgress from "nprogress";
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-  <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>
-  )
+    <>
+      <Head></Head>
+      <ChakraProvider>
+        <Layout>
+        <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
+  );
 }
