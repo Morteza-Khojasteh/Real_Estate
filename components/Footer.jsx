f61e0f7c4a6e80832fc5a0ad657ec399c99a814e
Mor Khojasteh <mori.kh1987@gmail.com>
2022-09-04 13:42:46 +0100

    feat: added new component for Footer

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => (
+  <Box
+    textAlign="center"
+    p="5"
+    fontSize="lg"
+    color="black.300"
+    borderTop="1px"
+    borderColor="gray"
+  >
+    &copy; 2022 Moria, Co
+  </Box>
+);
+
+export default Footer;
