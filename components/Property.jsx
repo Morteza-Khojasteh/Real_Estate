3ebe02c48bb9770edeef23dbee1030abc20faaf1
Mor Khojasteh <mori.kh1987@gmail.com>
2022-09-04 11:23:18 +0100

    feat: added new componet for property

---
+++ components/Property.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
+import { FaBed, FaBath } from "react-icons/fa";
+import { BsGridFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
+import millify from "millify";
+import DefaultImage from "../assets/images/house-image.jpg"
+
+const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId }}) => (
+    <Link href={`/Property/${externalId}`} passHref >
+        <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
+            <Box>
+                <Image src={coverPhoto ? coverPhoto.url : DefaultImage } width={400} height={260} alt="House" />
+            </Box>
+        </Flex>
+    </Link>
+);
+
+
+export default Property;
