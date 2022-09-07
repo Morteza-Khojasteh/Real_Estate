5dac5ae5bfb1e962c4adb280b7a6dfabd5fa6ce9
Mor Khojasteh <mori.kh1987@gmail.com>
2022-09-07 15:33:26 +0100

    feat: added property content below

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -6,24 +6,71 @@ import millify from "millify";
 import { baseUrl, fetchApi } from "../../utils/fetchApi";
 import ImageScrollbar from "../../components/ImageScrollbar";
 
-
-const PropertyDetails = ({ PropertyDetails : { price, rentFrequency, rooms, baths, area, title, agency, purpose, isVerified, description, type, furnishingStatus, amenities, photos } }) => (
+const PropertyDetails = ({
+  PropertyDetails: {
+    price,
+    rentFrequency,
+    rooms,
+    baths,
+    area,
+    title,
+    agency,
+    purpose,
+    isVerified,
+    description,
+    type,
+    furnishingStatus,
+    amenities,
+    photos,
+  },
+}) => (
   <Box maxWidth="1000px" margin="auto" p="4">
-    {photos && <ImageScrollbar  data={photos} />}
+    {photos && <ImageScrollbar data={photos} />}
+    <Box w="full" p="6">
+      <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+        <Flex alignItems="center">
+          <Box paddingRight="3" color="green.500">
+            {isVerified && <GoVerified />}
+          </Box>
+          <Text fontWeight="bold" fontSize="lg">
+            AED {millify(price)}
+            {rentFrequency && `/${rentFrequency}`}{" "}
+          </Text>
+        </Flex>
+        <Box>
+          <Avatar size="sm" src={agency?.logo?.url} />
+        </Box>
+      </Flex>
+      <Flex
+        alignItems="center"
+        p="1"
+        justifyContent="space-between"
+        w="250px"
+        color="blue.700"
+      >
+        {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{" "}
+        <BsGridFill />
+      </Flex>
+      <Box marginTop="2">
+        <Text fontSize="lg" marginBottom="2" fontWeight="bold">
+          {title}
+        </Text>
+        <Text lineHeight="2" color="gray.600">
+          {description}{" "}
+        </Text>
+      </Box>
+    </Box>
   </Box>
 );
 
 export default PropertyDetails;
 
 export async function getServerSideProps({ params: { id } }) {
-  const data = await fetchApi(
-    `${baseUrl}/properties/detail?externalID=${id}`
-  );
-
+  const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
 
   return {
     props: {
-      PropertyDetails : data
+      PropertyDetails: data,
     },
   };
 }
