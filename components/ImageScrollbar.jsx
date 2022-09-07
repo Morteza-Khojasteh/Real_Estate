395fcc7f1bdf6b1b1b415ca6f697ca21224c8626
Mor Khojasteh <mori.kh1987@gmail.com>
2022-09-07 15:17:26 +0100

    feat: added callBack function

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -11,7 +11,7 @@ const LeftArrow = () => {
     <Flex justifyContent="center" alignItems="center" marginRight="1">
       <Icon
         as={FaArrowAltCircleLeft}
-        onClick={scrollPrev}
+        onClick={()=>  scrollPrev()}
         fontSize="2xl"
         cursor="pointer"
       />
@@ -26,7 +26,7 @@ const RightArrow = () => {
     <Flex justifyContent="center" alignItems="center" marginRight="1">
       <Icon
         as={FaArrowAltCircleRight}
-        onClick={scrollNext}
+        onClick={()=> scrollNext()}
         fontSize="2xl"
         cursor="pointer"
       />
@@ -34,7 +34,8 @@ const RightArrow = () => {
   );
 };
 
-export default function ImageScrollbar({ data }) {
+function ImageScrollbar({ data }) {
+    
   return (
     <ScrollMenu
       LeftArrow={LeftArrow}
@@ -42,17 +43,21 @@ export default function ImageScrollbar({ data }) {
       style={{ overflow: "hidden" }}
     >
       {data.map((item) => (
-        <Box width="910px" itemId={item.id} overflow="hidden" p="1">
+        <Box width="910px" key={item.id} itemID={item.id} overflow="hidden" p="1">
           <Image
+            alt="property"
             placeholder="blur"
             blurDataURL={item.url}
             src={item.url}
             width={1000}
             height={500}
-            sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
+            sizes="(max-width: 500px) 100px, (max-width: 1024px) 400px, 1000px"
           />
         </Box>
       ))}
     </ScrollMenu>
   );
 }
+
+
+export default ImageScrollbar;
